cc857a1c86db0c39c50ee389a7d07e2adee195fd
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-02 18:09:06 -0300

    chore: added prefix

--- src/app/my-first-component/my-first-component.component.ts
+++ src/app/my-first-component/my-first-component.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-my-first-component',
+  selector: 'myFirst',
   templateUrl: './my-first-component.component.html',
   styleUrls: ['./my-first-component.component.scss']
 })
 export class MyFirstComponentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
